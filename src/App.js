847c309d0f0892cde795cae35e098ac1b1828cbb
Hivna <hivna12@gmail.com>
2024-05-31 11:56:52 -0300

    feat: add functions to switch game stages

--- src/App.js
+++ src/App.js
@@ -4,16 +4,42 @@ import './App.css';
 import { useCallback, useEffect, useState } from 'react';
 
 // Data
-import { wordslist } from "./data/words";
+import { wordsList } from "./data/words";
 
 // Components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 
+// Serão usados para fazer a progressão
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage ==='game' && <Game verifyLetter={verifyLetter}/> }
+      {gameStage ==='end' && <GameOver retry={retry} />}
      
     </div>
   );
