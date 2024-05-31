eff2a0c6af5c751103ee3ff4f33a58cc5b9e55e2
Hivna <hivna12@gmail.com>
2024-05-31 10:34:11 -0300

    chore: adjust files for project setup

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Gasolina", "Capô", "Pneu", "Antena", "Volante"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Uva", "Kiwi", "Morango", "Pêssego"],
+    corpo: ["Braço", "Perna", "Unhas", "Cérebro", "Pelos", "Pescoço", "Olhos", "Dedos", "Nariz", "Boca"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Ruby", "Java", "Vue"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Pão", "Farofa", "Macarrão"],
+  };
